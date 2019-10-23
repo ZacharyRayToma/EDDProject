94e104cbde1561886a7010bec6eba901b13ad7ea
Zachary Tomaszewski <34655891+ztoma2420@users.noreply.github.com>
2019-10-23 14:50:22 -0600

    added alot of stuff

---
+++ New Unity Project/Assets/Objects/Human Models/PlayerAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        //selects the animation state depending on the keys pressed
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_Animator.SetInteger("MotionState", 2);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            m_Animator.SetInteger("MotionState", 3);
+        }
+        else
+        {
+            m_Animator.SetInteger("MotionState", 1);
+        }
+    }
+}
