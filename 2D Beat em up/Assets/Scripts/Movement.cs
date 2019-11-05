197c36f12ba13b0cd36887f24d5105feeaad5835
Zachary Tomaszewski <34655891+ztoma2420@users.noreply.github.com>
2019-11-05 14:55:16 -0700

    Added left and right movement

---
+++ 2D Beat em up/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 position;
+    // Use this for initialization
+    void Start()
+    {
+        position = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && position.x < 100) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime, transform);
+        }
+        if (Input.GetKey(KeyCode.A) && position.x < 100)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime, transform);
+        }
+    }
+}
