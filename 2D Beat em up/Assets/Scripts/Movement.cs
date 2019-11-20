3247f9ed9645c716aeca28f17b0f404d2f3ed4a8
Zachary Tomaszewski <34655891+ztoma2420@users.noreply.github.com>
2019-11-20 14:54:36 -0700

    Yes

--- 2D Beat em up/Assets/Scripts/Movement.cs
+++ 2D Beat em up/Assets/Scripts/Movement.cs
@@ -7,20 +7,44 @@ public class Movement : MonoBehaviour
     public float speed;
     private Vector2 position;
     public Animator animator;
+    public string moveLock;
+    public int maxlock;
     // Use this for initialization
     void Start()
     {
         position = gameObject.transform.position;
         animator = GetComponent<Animator>();
-        
+        moveLock = null;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D) && position.x < 100) {
-            transform.Translate(Vector2.right * speed * Time.deltaTime, transform);
-            animator.SetInteger("AnimationState", 1);
+        
+        if (moveLock == "dashforward" || Input.GetKey(KeyCode.D)) {
+            if (moveLock == "dashforward" || Input.GetKey(KeyCode.LeftShift))
+            {
+                maxlock = 30;
+                transform.Translate(Vector2.right * speed*5 * Time.deltaTime, transform);
+                animator.SetInteger("AnimationState", 3);
+                for(int locktime = 0; locktime < maxlock; locktime++)
+                {
+                    if(locktime < maxlock - 1)
+                    {
+                        moveLock = "dashforward";
+                    }
+                    else
+                    {
+                        moveLock = null;
+                    }
+                }
+            }
+            else
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime, transform);
+                animator.SetInteger("AnimationState", 1);
+            }
         }
         else if (Input.GetKey(KeyCode.A) && position.x < 100)
         {
